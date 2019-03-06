1b074972be8f6ac1156a46366dc10b874c68132c
AlexSantana-Met <alejandro.sant12.12@gmail.com>
2019-03-06 14:20:19 -0600

    :alien: Funcionamiento vista Ponentes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -7,47 +7,6 @@
     <div class="container-fluid">
         <div id="carouselSponsor" class="carousel slide my-1" data-ride="carousel" data-interval="3000">
             <div class="carousel-inner" role="listbox" id="carousel-sponsor-items">
-
-                <div class="carousel-item active">
-                    <div class="row centrar">
-                        <a href="#" title="image 1" class="col">
-                            <img class="img-fluid mx-auto d-block" src="img/96090.jpg" alt="slide 1"
-                                 onclick="console.log(this.alt)">
-                        </a>
-                        <a href="#" title="image 2" class="col">
-                            <img class="img-fluid mx-auto d-block" src="img/BqJrLh7.jpg" alt="slide 2"
-                                 onclick="console.log(this.alt)">
-                        </a>
-                        <a href="#" title="image 3" class="col">
-                            <img class="img-fluid mx-auto d-block" src="img/GkVzOEL.jpg" alt="slide 3"
-                                 onclick="console.log(this.alt)">
-                        </a>
-                        <a href="#" title="image 4" class="col">
-                            <img class="img-fluid mx-auto d-block" src="img/PjDzgYx.jpg" alt="slide 4"
-                                 onclick="console.log(this.alt)">
-                        </a>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="row centrar">
-                        <a href="#" title="image 5" class="col">
-                            <img class="img-fluid mx-auto d-block" src="img/dhRVvtA.jpg" alt="slide 5"
-                                 onclick="console.log(this.alt)">
-                        </a>
-                        <a href="#" title="image 6" class="col">
-                            <img class="img-fluid mx-auto d-block" src="img/hf1K1zu.jpg" alt="slide 6"
-                                 onclick="console.log(this.alt)">
-                        </a>
-                        <a href="#" title="image 7" class="col">
-                            <img class="img-fluid mx-auto d-block" src="img/z61PIz2.jpg" alt="slide 7"
-                                 onclick="console.log(this.alt)">
-                        </a>
-                        <a href="#" title="image 8" class="col">
-                            <img class="img-fluid mx-auto d-block" src="img/flisol.png" alt="slide 8"
-                                 onclick="console.log(this.alt)">
-                        </a>
-                    </div>
-                </div>
             </div>
         </div>
     </div>

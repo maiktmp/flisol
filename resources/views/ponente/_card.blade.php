fcb6cf2827daec4db3c2f53e5b7c72e8b08cb8aa
maiktmp <maiktmp@gmail.com>
2019-03-21 23:44:53 -0600

    :alien: Detalles de los eventos de los ponentes

--- resources/views/ponente/_card.blade.php
+++ resources/views/ponente/_card.blade.php
@@ -1,6 +1,6 @@
 <div class="col-4 mx-2">
     <div class="box">
-        <div class="card bg-dark text-white text-left card-shadow" style="width:250px">
+        <div class="card bg-dark text-white text-left card-shadow" style="">
             <img class="card-img-top img-card-center" src="{{asset ($img_url)}}" alt="Responsive">
             <div class="card-body">
                 <h5 class="card-title">{{$name}}</h5>
